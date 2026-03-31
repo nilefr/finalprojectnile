e172365115e06c16709be058161266a7858c14c5
frnile <niledunning910@gmail.com>
2026-03-31 11:27:48 -0700

    routes

--- finalprojectnile/vite.config.js
+++ finalprojectnile/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'    // <-- ADD THIS
+ 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],        	// <-- ADD 
 })
+
