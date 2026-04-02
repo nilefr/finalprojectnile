dab8fbaf0cd6b7957e2029fefc760fd942774f89
frnile <niledunning910@gmail.com>
2026-04-02 14:26:41 -0700

    fixed the importing issues on school computer

--- finalprojectnile/src/App.jsx
+++ finalprojectnile/src/App.jsx
@@ -6,12 +6,12 @@ import Profile from "./pages/Profile";
 import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
 import Search from "./components/Search.jsx";
-import Hero from "./components/Hero.jsx";
-import Carousel from "./components/Carousel.jsx";
-import Card from "./components/Card.jsx";
+
+
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
         <NavBar />
@@ -27,21 +27,13 @@ function App() {
         <Footer />
       </div>
     </BrowserRouter>
-    
     <Search onSearch={(query) => console.log("Search query:", query)} />
+    
+    
 
-   <Hero title="Welcome to My Site" subtitle="This is a hero section" backgroundImage="https://i.ytimg.com/vi/RJy_AL-zzTQ/maxresdefault.jpg" />
+   
+   
     
-    <Carousel
-      images={[
-        "https://i.ytimg.com/vi/RJy_AL-zzTQ/maxresdefault.jpg",
-        "https://cdn.mos.cms.futurecdn.net/2a9e7b1c8c5f0e3b4d9f8a1b2c3d4e5.jpg",
-        "https://www.nasa.gov/sites/default/files/thumbnails/image/potw2046a.jpg"
-      ]}
-      caption="Amazing Space Images"
-    />
-    
-    <Card />
     
     </>
   )
