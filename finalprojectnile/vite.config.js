9b7bf43516b2d2a31224d78569f8ffab019dee88
nilefr <niledunning910@gmail.com>
2026-05-03 19:18:35 -0700

    Add base path to Vite configuration

--- finalprojectnile/vite.config.js
+++ finalprojectnile/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'    // <-- ADD THIS
  
 export default defineConfig({
-  plugins: [react(), tailwindcss()],        	// <-- ADD 
+  plugins: [react(), tailwindcss()], 
+  base: '/finalprojectnile/',// <-- ADD 
 })
 
